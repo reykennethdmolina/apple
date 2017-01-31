14fde445b3012b419f592212802e8747fd3ac880
Ken Molina <reykennethdmolina@gmail.com>
2017-01-31 23:56:05 +0800

    journalvoucher

--- financial/journalvoucher/views.py
+++ financial/journalvoucher/views.py
@@ -4,6 +4,8 @@ from django.utils.decorators import method_decorator
 from django.http import HttpResponseRedirect
 from jvtype.models import Jvtype
 from currency.models import Currency
+from branch.models import Branch
+from department.models import Department
 from bank.models import Bank
 import datetime
 
@@ -17,6 +19,8 @@ class CreateView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super(CreateView, self).get_context_data(**kwargs)
+        context['department'] = Department.objects.filter(isdeleted=0).order_by('pk')
+        context['branch'] = Branch.objects.filter(isdeleted=0).order_by('pk')
         context['currency'] = Currency.objects.filter(isdeleted=0).order_by('pk')
         context['jvtype'] = Jvtype.objects.filter(isdeleted=0).order_by('pk')
         return context
