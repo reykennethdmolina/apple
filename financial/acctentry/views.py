8b0e3f06a18d97005a29003b29c2923d29e26a1c
Ken Molina <reykennethdmolina@gmail.com>
2017-02-22 14:30:04 +0800

    Merge branch 'kenmolina' of https://github.com/reykennethdmolina/apple into kenmolina
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- financial/acctentry/views.py
+++ financial/acctentry/views.py
@@ -4,7 +4,19 @@ from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 from chartofaccount.models import Chartofaccount
-from bank.models import Bank
+from bankaccount.models import Bankaccount
+from department.models import Department
+from employee.models import Employee
+from supplier.models import Supplier
+from customer.models import Customer
+from unit.models import Unit
+from branch.models import Branch
+from product.models import Product
+from inputvat.models import Inputvat
+from outputvat.models import Outputvat
+from vat.models import Vat
+from wtax.models import Wtax
+from ataxcode.models import Ataxcode
 
 import json
 
@@ -16,6 +28,18 @@ def maccountingentry(request):
         #context['chartofaccount'] = Chartofaccount.objects.filter(isdeleted=0).order_by('accountcode')
         context = {
             'chartofaccount':  Chartofaccount.objects.filter(isdeleted=0).order_by('accountcode'),
+            'bankaccount':  Bankaccount.objects.filter(isdeleted=0).order_by('code'),
+            'department':  Department.objects.filter(isdeleted=0).order_by('departmentname'),
+            'employee':  Employee.objects.filter(isdeleted=0).order_by('firstname', 'lastname'),
+            'supplier':  Supplier.objects.filter(isdeleted=0).order_by('name'),
+            'customer':  Customer.objects.filter(isdeleted=0).order_by('name'),
+            'branch':  Branch.objects.filter(isdeleted=0).order_by('description'),
+            'product':  Product.objects.filter(isdeleted=0).order_by('description'),
+            'inputvat':  Inputvat.objects.filter(isdeleted=0).order_by('description'),
+            'outputvat':  Outputvat.objects.filter(isdeleted=0).order_by('description'),
+            'vat':  Vat.objects.filter(isdeleted=0).order_by('description'),
+            'wtax':  Wtax.objects.filter(isdeleted=0).order_by('description'),
+            'ataxcode':  Ataxcode.objects.filter(isdeleted=0).order_by('description'),
         }
         data = {
             'status': 'success',
@@ -34,11 +58,20 @@ def checkchartvalidatetion(request):
     if request.method == 'POST':
         chartid = request.POST['chartid']
         chartdata = Chartofaccount.objects.filter(pk=chartid)
-        bank = Bank.objects.filter(isdeleted=0)
+
+        mainunit = 0
+        unit = []
+        if chartdata[0].unit_enable == 'Y':
+            mainunit = Chartofaccount.objects.values('mainunit').filter(pk=chartid)
+            unit = Unit.objects.filter(mainunit=mainunit)
+
+        #bank = Bank.objects.filter(isdeleted=0)
+        #print unit
         data = {
             'status': 'success',
             'chart': serializers.serialize("json", chartdata),
-            'bank': serializers.serialize("json", bank),
+            'unit': serializers.serialize("json", unit),
+            #'bank': serializers.serialize("json", bank),
         }
     else:
         data = {
