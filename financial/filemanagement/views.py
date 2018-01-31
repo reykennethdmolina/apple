4d62c63d7a82741d07a3f6feedae2e85cf39956d
Kelvin Barsana <kelvin.tres@yahoo.com>
2018-01-31 16:43:53 +0800

    File Management - JV

--- financial/filemanagement/views.py
+++ financial/filemanagement/views.py
@@ -14,13 +14,14 @@ class IndexView(TemplateView):
 
 	def get_context_data(self, **kwargs):
 		context = super(TemplateView, self).get_context_data(**kwargs)
-		context['f_total'], context['f_or'], context['f_agent'], context['f_agency'], context['f_client'] = get_size()
+		context['f_total'], context['f_or'], context['f_agent'], context['f_agency'], context['f_client'], context['f_jv'] = get_size()
 
 		if context['f_total'] != 0:
 			context['fp_or'] = round(context['f_or'] / context['f_total'] * 100 , 2)
 			context['fp_agent'] = round(context['f_agent'] / context['f_total'] * 100 , 2)
 			context['fp_agency'] = round(context['f_agency'] / context['f_total'] * 100 , 2)
 			context['fp_client'] = round(context['f_client'] / context['f_total'] * 100 , 2)
+			context['fp_jv'] = round(context['f_jv'] / context['f_total'] * 100 , 2)
 
 		return context
 
@@ -31,6 +32,7 @@ def get_size(start_path = settings.MEDIA_ROOT):
 	f_agent = 0
 	f_agency = 0
 	f_client = 0
+	f_jv = 0
 
 	for dirpath, dirnames, filenames in os.walk(start_path):
 		for f in filenames:
@@ -45,14 +47,18 @@ def get_size(start_path = settings.MEDIA_ROOT):
 				f_agency += os.path.getsize(fp)
 			elif str(start_path) + '\processing_data\imported_client' in str(dirpath):
 				f_client += os.path.getsize(fp)
+			elif str(start_path) + '\processing_jv' in str(dirpath):
+				f_jv += os.path.getsize(fp)
+
 
 	f_total = roundBytes(f_total, 'mb')
 	f_or = roundBytes(f_or, 'mb')
 	f_agent = roundBytes(f_agent, 'mb')
 	f_agency = roundBytes(f_agency, 'mb')
 	f_client = roundBytes(f_client, 'mb')
+	f_jv = roundBytes(f_jv, 'mb')
 
-	return f_total, f_or, f_agent, f_agency, f_client
+	return f_total, f_or, f_agent, f_agency, f_client, f_jv
 
 
 def truncateDir(request, path):
@@ -71,12 +77,22 @@ def truncateDir(request, path):
 		shutil.rmtree(settings.MEDIA_ROOT + '/' + path)
 		os.mkdir(settings.MEDIA_ROOT + '/' + path)
 
+	elif path == 'imported_jv':
+		path = 'processing_jv/imported_main' 
+		shutil.rmtree(settings.MEDIA_ROOT + '/' + path)
+		os.mkdir(settings.MEDIA_ROOT + '/' + path)
+		path = 'processing_jv/imported_detail' 
+		shutil.rmtree(settings.MEDIA_ROOT + '/' + path)
+		os.mkdir(settings.MEDIA_ROOT + '/' + path)
+
 	elif path == 'all':
 		path = ['processing_data/imported_client', 
 				'processing_data/imported_agent', 
 				'processing_data/imported_agency', 
 				'processing_or/imported_main', 
-				'processing_or/imported_detail']
+				'processing_or/imported_detail',
+				'processing_jv/imported_main', 
+				'processing_jv/imported_detail']
 		for data in path:
 			shutil.rmtree(settings.MEDIA_ROOT + '/' + data)
 			os.mkdir(settings.MEDIA_ROOT + '/' + data)
