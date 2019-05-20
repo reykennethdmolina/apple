95a37aee6db3b5fb97e7f7a9a5f8cf32dcce0de1
reykennethdmolina@gmail.com <reykennethdmolina@gmail.com>
2019-05-20 12:31:36 +0800

    update general ledger inquiry

--- financial/generaljournalbook/views.py
+++ financial/generaljournalbook/views.py
@@ -902,25 +902,54 @@ def transgenerate(request):
     chart = request.GET["chart"]
     transtatus = request.GET["transtatus"]
     status = request.GET["status"]
+    payeecode = request.GET["payeecode"]
+    payeename = request.GET["payeename"]
 
     context = {}
 
     print "transaction listing"
 
-    context['result'] = query_transaction(dto, dfrom, chart, transtatus, status)
+    ndto = datetime.datetime.strptime(dto, "%Y-%m-%d")
+    todate = datetime.date(int(ndto.year), int(ndto.month), 10)
+    toyear = todate.year
+    tomonth = todate.month
+    nfrom = datetime.datetime.strptime(dfrom, "%Y-%m-%d")
+    fromdate = datetime.date(int(nfrom.year), int(nfrom.month), 10)
+    fromyear = fromdate.year
+    frommonth = fromdate.month
+
+    prevdate = datetime.date(int(fromyear), int(frommonth), 10) - timedelta(days=15)
+    prevyear = prevdate.year
+    prevmonth = prevdate.month
+
+    begbalamount = 0
+    endbalamount = 0
+    endcode = ''
+
+    begbal =Subledgersummary.objects.filter(chartofaccount_id=chart, year=prevyear, month=prevmonth).first();
+    if begbal:
+        begbalamount = begbal.end_amount
+
+    endbal =Subledgersummary.objects.filter(chartofaccount_id=chart, year=toyear, month=tomonth).first();
+    if endbal:
+        endbalamount = endbal.end_amount
+        endcode = endbal.end_code
+
+    context['result'] = query_transaction(dto, dfrom, chart, transtatus, status, payeecode, payeename)
     context['dto'] = dto
     context['dfrom'] = dfrom
     viewhtml = render_to_string('generaljournalbook/transaction_result.html', context)
 
-
     data = {
         'status': 'success',
         'viewhtml': viewhtml,
+        'begbal': float(begbalamount),
+        'endbal': float(endbalamount),
+        'endcode': endcode
     }
     return JsonResponse(data)
 
-
-def query_transaction(dto, dfrom, chart, transtatus, status):
+def query_transaction(dto, dfrom, chart, transtatus, status, payeecode, payeename):
     print "Transaction Query"
     ''' Create query '''
     cursor = connection.cursor()
@@ -931,6 +960,13 @@ def query_transaction(dto, dfrom, chart, transtatus, status):
     chart_transtatus_jv = ''
     chart_transtatus_or = ''
     chart_status = ''
+    chart_payeecode1 = ''
+    chart_payeecode2 = ''
+    chart_payeecode3 = ''
+    chart_payeename1 = ''
+    chart_payeename2 = ''
+    chart_payeename3 = ''
+
     if chart != '':
         chart_condition = "AND d.chartofaccount_id = '" + str(chart) + "'"
     if transtatus != '':
@@ -940,47 +976,49 @@ def query_transaction(dto, dfrom, chart, transtatus, status):
         chart_transtatus_or = "AND m.orstatus = '" + str(transtatus) + "'"
     if status != '':
         chart_status = "AND m.status = '" + str(status) + "'"
-    # elif chart != '' and chart2 == '':
-    #     condition = "AND chart.accountcode = '" + str(chart) + "'"
-    # elif chart2 != '' and chart == '':
-    #     condition = "AND chart.accountcode = '" + str(chart2) + "'"
+    if payeecode != '':
+        chart_payeecode1 = "AND m.payeecode LIKE '%" + str(payeecode) + "%'"
+        chart_payeecode2 = "AND m.payee_code LIKE '%" + str(payeecode) + "%'"
+    if payeename != '':
+        chart_payeename1 = "AND m.payeename LIKE '%" + str(payeename) + "%'"
+        chart_payeename2 = "AND m.payee_name LIKE '%" + str(payeename) + "%'"
 
     query = "SELECT z.tran, z.item_counter, z.ap_num AS tnum, z.ap_date AS tdate, z.debitamount, z.creditamount, z.balancecode, z.apstatus AS transtatus, z.status AS status, bank.code AS bank, chart.accountcode, chart.description AS chartofaccount, cust.code AS custcode, cust.name AS customer, dept.code AS deptcode, dept.departmentname AS department, " \
             "emp.code AS empcode, CONCAT(IFNULL(emp.firstname, ''), ' ', IFNULL(emp.lastname, '')) AS employee, inpvat.code AS inpvatcode, inpvat.description AS inputvat, " \
             "outvat.code AS outvatcode, outvat.description AS outputvat, prod.code AS prodcode, prod.description AS product, " \
-            "supp.code AS suppcode, supp.name AS supplier, vat.code AS vatcode, vat.description AS vat, wtax.code AS wtaxcode, wtax.description AS wtax " \
+            "supp.code AS suppcode, supp.name AS supplier, vat.code AS vatcode, vat.description AS vat, wtax.code AS wtaxcode, wtax.description AS wtax, z.payeecode AS payee_code, z.payeename AS payee_name, z.particulars " \
             "FROM ( " \
             "SELECT 'AP' AS tran, d.item_counter, d.ap_num, d.ap_date, d.debitamount, d.creditamount, d.balancecode, d.ataxcode_id, " \
             "d.bankaccount_id, d.branch_id, d.chartofaccount_id, d.customer_id, d.department_id, d.employee_id, d.inputvat_id, " \
-            "d.outputvat_id, d.product_id, d.supplier_id, d.vat_id, d.wtax_id, m.apstatus, m.status	 " \
+            "d.outputvat_id, d.product_id, d.supplier_id, d.vat_id, d.wtax_id, m.apstatus, m.status, m.payeecode, m.payeename, m.particulars	 " \
             "FROM apdetail AS d " \
             "LEFT OUTER JOIN apmain AS m ON m.id = d.apmain_id " \
             "WHERE DATE(d.ap_date) >= '"+str(dfrom)+"' AND DATE(d.ap_date) <= '"+str(dto)+"' " \
-            +str(chart_condition)+" "+str(chart_transtatus_ap)+" "+str(chart_status)+"" \
+            +str(chart_condition)+" "+str(chart_transtatus_ap)+" "+str(chart_status)+" "+str(chart_payeecode1)+" "+str(chart_payeename1)+"" \
             "UNION " \
             "SELECT 'CV' AS tran, d.item_counter, d.cv_num, d.cv_date, d.debitamount, d.creditamount, d.balancecode, d.ataxcode_id, " \
             "d.bankaccount_id, d.branch_id, d.chartofaccount_id, d.customer_id, d.department_id, d.employee_id, d.inputvat_id, " \
-            "d.outputvat_id, d.product_id, d.supplier_id, d.vat_id, d.wtax_id, m.cvstatus, m.status	 " \
+            "d.outputvat_id, d.product_id, d.supplier_id, d.vat_id, d.wtax_id, m.cvstatus, m.status, m.payee_code, m.payee_name, m.particulars	 " \
             "FROM cvdetail AS d " \
             "LEFT OUTER JOIN cvmain AS m ON m.id = d.cvmain_id " \
             "WHERE DATE(d.cv_date) >= '"+str(dfrom)+"' AND DATE(d.cv_date) <= '"+str(dto)+"' " \
-            +str(chart_condition)+" "+str(chart_transtatus_cv)+" "+str(chart_status)+"" \
+            +str(chart_condition)+" "+str(chart_transtatus_cv)+" "+str(chart_status)+" "+str(chart_payeecode2)+" "+str(chart_payeename2)+"" \
             "UNION " \
             "SELECT 'JV' AS tran, d.item_counter, d.jv_num, d.jv_date, d.debitamount, d.creditamount, d.balancecode, d.ataxcode_id, " \
             "d.bankaccount_id, d.branch_id, d.chartofaccount_id, d.customer_id, d.department_id, d.employee_id, d.inputvat_id, " \
-            "d.outputvat_id, d.product_id, d.supplier_id, d.vat_id, d.wtax_id, m.jvstatus, m.status	 " \
+            "d.outputvat_id, d.product_id, d.supplier_id, d.vat_id, d.wtax_id, m.jvstatus, m.status, '' AS payeecode, '' AS payeename, m.particular	 " \
             "FROM jvdetail AS d " \
             "LEFT OUTER JOIN jvmain AS m ON m.id = d.jvmain_id " \
             "WHERE DATE(d.jv_date) >= '"+str(dfrom)+"' AND DATE(d.jv_date) <= '"+str(dto)+"' " \
-            +str(chart_condition)+" "+str(chart_transtatus_jv)+" "+str(chart_status)+"" \
+            +str(chart_condition)+" "+str(chart_transtatus_jv)+" "+str(chart_status)+" "+str(chart_payeecode3)+" "+str(chart_payeename3)+"" \
             "UNION " \
             "SELECT 'OR' AS tran, d.item_counter, m.ornum, m.ordate, d.debitamount, d.creditamount, d.balancecode, d.ataxcode_id, " \
             "d.bankaccount_id, d.branch_id, d.chartofaccount_id, d.customer_id, d.department_id, d.employee_id, d.inputvat_id, " \
-            "d.outputvat_id, d.product_id, d.supplier_id, d.vat_id, d.wtax_id, m.orstatus, m.status	" \
+            "d.outputvat_id, d.product_id, d.supplier_id, d.vat_id, d.wtax_id, m.orstatus, m.status, m.payee_code, m.payee_name, m.particulars	" \
             "FROM ormain AS m " \
             "LEFT OUTER JOIN ordetail AS d ON m.id = d.ormain_id " \
             "WHERE DATE(m.ordate) >= '"+str(dfrom)+"' AND DATE(m.ordate) <= '"+str(dto)+"' " \
-            +str(chart_condition)+" "+str(chart_transtatus_or)+" "+str(chart_status)+") AS z " \
+            +str(chart_condition)+" "+str(chart_transtatus_or)+" "+str(chart_status)+" "+str(chart_payeecode2)+" "+str(chart_payeename2)+") AS z " \
             "LEFT OUTER JOIN bankaccount AS bank ON bank.id = z.bankaccount_id " \
             "LEFT OUTER JOIN chartofaccount AS chart ON chart.id = z.chartofaccount_id " \
             "LEFT OUTER JOIN customer AS cust ON cust.id = z.customer_id " \
@@ -992,8 +1030,8 @@ def query_transaction(dto, dfrom, chart, transtatus, status):
             "LEFT OUTER JOIN supplier AS supp ON supp.id = z.supplier_id " \
             "LEFT OUTER JOIN vat AS vat ON vat.id = z.vat_id " \
             "LEFT OUTER JOIN wtax AS wtax ON wtax.id = z.wtax_id " \
-            "ORDER BY z.tran, z.ap_num, z.ap_date, z.item_counter"
-
+            "ORDER BY z.ap_date, z.ap_num, z.tran, z.item_counter"
+    #print query
     cursor.execute(query)
     result = namedtuplefetchall(cursor)
 
@@ -1009,10 +1047,38 @@ class TransExcel(View):
         chart = request.GET["chart"]
         transtatus = request.GET["transtatus"]
         status = request.GET["status"]
+        payeecode = request.GET["payeecode"]
+        payeename = request.GET["payeename"]
 
         print "transaction listing"
 
-        result = query_transaction(dto, dfrom, chart, transtatus, status)
+        ndto = datetime.datetime.strptime(dto, "%Y-%m-%d")
+        todate = datetime.date(int(ndto.year), int(ndto.month), 10)
+        toyear = todate.year
+        tomonth = todate.month
+        nfrom = datetime.datetime.strptime(dfrom, "%Y-%m-%d")
+        fromdate = datetime.date(int(nfrom.year), int(nfrom.month), 10)
+        fromyear = fromdate.year
+        frommonth = fromdate.month
+
+        prevdate = datetime.date(int(fromyear), int(frommonth), 10) - timedelta(days=15)
+        prevyear = prevdate.year
+        prevmonth = prevdate.month
+
+        begbalamount = 0
+        endbalamount = 0
+        endcode = ''
+
+        begbal = Subledgersummary.objects.filter(chartofaccount_id=chart, year=prevyear, month=prevmonth).first();
+        if begbal:
+            begbalamount = begbal.end_amount
+
+        endbal = Subledgersummary.objects.filter(chartofaccount_id=chart, year=toyear, month=tomonth).first();
+        if endbal:
+            endbalamount = endbal.end_amount
+            endcode = endbal.end_code
+
+        result = query_transaction(dto, dfrom, chart, transtatus, status, payeecode, payeename)
 
         # Create an in-memory output file for the new workbook.
         output = io.BytesIO()
@@ -1029,35 +1095,43 @@ class TransExcel(View):
         worksheet.write('A1', 'GENERAL LEDGER TRANSACTION LISTING', bold)
         worksheet.write('A2', 'AS OF ' + str(dfrom) + ' to ' + str(dto), bold)
 
+        worksheet.write('A3', 'Beg Bal', bold)
+        worksheet.write('B3', float(format(begbalamount, '.2f')))
+        worksheet.write('C3', 'End Bal', bold)
+        worksheet.write('A4', float(format(endbalamount, '.2f')))
+        worksheet.write('B4', 'Balance Code', bold)
+        worksheet.write('C4', endcode, bold)
+
         # header
         worksheet.write('A4', 'Type', bold)
         worksheet.write('B4', 'Number', bold)
         worksheet.write('C4', 'Date', bold)
-        worksheet.write('D4', 'Account Code', bold)
-        worksheet.write('E4', 'Chart of Account', bold)
-        worksheet.write('F4', 'Debit', bold)
-        worksheet.write('G4', 'Credit', bold)
-        worksheet.write('H4', 'Transaction Status', bold)
-        worksheet.write('I4', 'Status', bold)
-        worksheet.write('J4', 'Bank Account', bold)
-        worksheet.write('K4', 'Customer Code', bold)
-        worksheet.write('L4', 'Customer', bold)
-        worksheet.write('M4', 'Supplier Code', bold)
-        worksheet.write('N4', 'Supplier', bold)
-        worksheet.write('O4', 'Department Code', bold)
-        worksheet.write('P4', 'Department', bold)
-        worksheet.write('Q4', 'Employee Code', bold)
-        worksheet.write('R4', 'Employee', bold)
-        worksheet.write('S4', 'Product Code', bold)
-        worksheet.write('T4', 'Product', bold)
-        worksheet.write('U4', 'VAT Code', bold)
-        worksheet.write('V4', 'VAT', bold)
-        worksheet.write('W4', 'WTAX Code', bold)
-        worksheet.write('X4', 'WTAX', bold)
-        worksheet.write('Y4', 'Input VAT Code', bold)
-        worksheet.write('Z4', 'Input VAT', bold)
-        worksheet.write('AA4', 'Output VAT Code', bold)
-        worksheet.write('AB4', 'Output VAT', bold)
+        worksheet.write('D4', 'Particulars', bold)
+        worksheet.write('E4', 'Account Code', bold)
+        worksheet.write('F4', 'Chart of Account', bold)
+        worksheet.write('G4', 'Debit', bold)
+        worksheet.write('H4', 'Credit', bold)
+        worksheet.write('I4', 'Transaction Status', bold)
+        worksheet.write('J4', 'Status', bold)
+        worksheet.write('K4', 'Bank Account', bold)
+        worksheet.write('L4', 'Customer Code', bold)
+        worksheet.write('M4', 'Customer', bold)
+        worksheet.write('N4', 'Supplier Code', bold)
+        worksheet.write('O4', 'Supplier', bold)
+        worksheet.write('P4', 'Department Code', bold)
+        worksheet.write('Q4', 'Department', bold)
+        worksheet.write('R4', 'Employee Code', bold)
+        worksheet.write('S4', 'Employee', bold)
+        worksheet.write('T4', 'Product Code', bold)
+        worksheet.write('U4', 'Product', bold)
+        worksheet.write('V4', 'VAT Code', bold)
+        worksheet.write('W4', 'VAT', bold)
+        worksheet.write('X4', 'WTAX Code', bold)
+        worksheet.write('Y4', 'WTAX', bold)
+        worksheet.write('Z4', 'Input VAT Code', bold)
+        worksheet.write('AA4', 'Input VAT', bold)
+        worksheet.write('AB4', 'Output VAT Code', bold)
+        worksheet.write('AC4', 'Output VAT', bold)
 
 
         row = 5
@@ -1069,31 +1143,32 @@ class TransExcel(View):
             worksheet.write(row, col, data.tran)
             worksheet.write(row, col + 1, data.tnum)
             worksheet.write(row, col + 2, data.tdate, formatdate)
-            worksheet.write(row, col + 3, data.accountcode)
-            worksheet.write(row, col + 4, data.chartofaccount)
-            worksheet.write(row, col + 5, float(format(data.debitamount, '.2f')))
-            worksheet.write(row, col + 6, float(format(data.creditamount, '.2f')))
-            worksheet.write(row, col + 7, data.transtatus)
-            worksheet.write(row, col + 8, data.status)
-            worksheet.write(row, col + 9, data.bank)
-            worksheet.write(row, col + 10, data.custcode)
-            worksheet.write(row, col + 11, data.customer)
-            worksheet.write(row, col + 12, data.suppcode)
-            worksheet.write(row, col + 13, data.supplier)
-            worksheet.write(row, col + 14, data.deptcode)
-            worksheet.write(row, col + 15, data.department)
-            worksheet.write(row, col + 16, data.empcode)
-            worksheet.write(row, col + 17, data.employee)
-            worksheet.write(row, col + 18, data.prodcode)
-            worksheet.write(row, col + 19, data.product)
-            worksheet.write(row, col + 20, data.vatcode)
-            worksheet.write(row, col + 21, data.vat)
-            worksheet.write(row, col + 22, data.wtaxcode)
-            worksheet.write(row, col + 23, data.wtax)
-            worksheet.write(row, col + 24, data.inpvatcode)
-            worksheet.write(row, col + 25, data.inputvat)
-            worksheet.write(row, col + 26, data.outvatcode)
-            worksheet.write(row, col + 27, data.outputvat)
+            worksheet.write(row, col + 3, data.particulars)
+            worksheet.write(row, col + 4, data.accountcode)
+            worksheet.write(row, col + 5, data.chartofaccount)
+            worksheet.write(row, col + 6, float(format(data.debitamount, '.2f')))
+            worksheet.write(row, col + 7, float(format(data.creditamount, '.2f')))
+            worksheet.write(row, col + 8, data.transtatus)
+            worksheet.write(row, col + 9, data.status)
+            worksheet.write(row, col + 10, data.bank)
+            worksheet.write(row, col + 11, data.custcode)
+            worksheet.write(row, col + 12, data.customer)
+            worksheet.write(row, col + 13, data.suppcode)
+            worksheet.write(row, col + 14, data.supplier)
+            worksheet.write(row, col + 15, data.deptcode)
+            worksheet.write(row, col + 16, data.department)
+            worksheet.write(row, col + 17, data.empcode)
+            worksheet.write(row, col + 18, data.employee)
+            worksheet.write(row, col + 19, data.prodcode)
+            worksheet.write(row, col + 20, data.product)
+            worksheet.write(row, col + 21, data.vatcode)
+            worksheet.write(row, col + 22, data.vat)
+            worksheet.write(row, col + 23, data.wtaxcode)
+            worksheet.write(row, col + 24, data.wtax)
+            worksheet.write(row, col + 25, data.inpvatcode)
+            worksheet.write(row, col + 26, data.inputvat)
+            worksheet.write(row, col + 27, data.outvatcode)
+            worksheet.write(row, col + 28, data.outputvat)
             row += 1
 
 
