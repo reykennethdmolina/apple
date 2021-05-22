03b3124f0566ed8b8c38fc24e8b4b077bdbcc96b
reykennethdmolina@gmail.com <reykennethdmolina@gmail.com>
2021-05-22 18:01:37 +0800

    update login logs

--- financial/financial/views.py
+++ financial/financial/views.py
@@ -7,8 +7,6 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect, Http404
 from module.models import Activitylogs
 
-
-
 def logout(request):
     print ('log out')
     # Save Activity Logs
@@ -25,6 +23,15 @@ def logout(request):
 @login_required
 def index(request):
     context_dict = {}
+
+    print ('log in')
+    # Save Activity Logs
+    Activitylogs.objects.create(
+        user_id=request.user.id,
+        username=request.user,
+        remarks='Log-in'
+    )
+
     return render(request, 'base-layout.html', context_dict)
     #return render(request, 'base-form.html', context_dict)
     #return HttpResponse("Welcome to Inquirer Enterprise Solutions - Financial System")
