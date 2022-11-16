20c837dd65fda4ce2091ae1676a0e296526d967b
reykennethdmolina@gmail.com <reykennethdmolina@gmail.com>
2022-11-16 13:31:55 +0800

    add taxespayable local - to repo

--- financial/taxespayable/urls.py
+++ financial/taxespayable/urls.py
@@ -1,4 +1,4 @@
-#__author__ = 'kelvin'
+#__author__ = 'ken'
 
 from django.conf.urls import url
 from . import views
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^transgenerate/$', views.transgenerate, name='transgenerate'),
-    url(r'^tagging/$', views.tagging, name='tagging'),
+    # url(r'^tagging/$', views.tagging, name='tagging'),
     url(r'^transexcel/$', views.TransExcel.as_view(), name='transexcel'),
-    url(r'^pdf/$', views.GeneratePDF.as_view(), name='pdf'),
+    # url(r'^pdf/$', views.GeneratePDF.as_view(), name='pdf'),
 ]
