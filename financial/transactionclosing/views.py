2683a653331bd588c5674e2954b705ec8b4fe4c4
unknown <reykennethdmolina@gmail.com>
2023-10-16 14:13:12 +0800

    fix transactionclosing

--- financial/transactionclosing/views.py
+++ financial/transactionclosing/views.py
@@ -546,7 +546,7 @@ def proc_updateclosing(request):
                                                              closedate__isnull=True).update(closeby=request.user,
                                                                                             closedate=datetime.datetime.now())
 
-        Companyparameter.objects.all().update(last_closed_date=newdt, closing = 1)
+        Companyparameter.objects.all().update(last_closed_date=newdt, closing = 0)
 
         data = {
             'status': 'success',
